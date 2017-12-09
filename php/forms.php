1043f606a0e4ac6ceca63e0f25ccf68a29e513fe
hkoscielski <huberto1010@op.pl>
2017-12-09 21:40:11 +0100

    superglobal, backslash in string, bug fixes

--- php/forms.php
+++ php/forms.php
@@ -7,10 +7,15 @@
 		body {
 			background-color: #404d43;
 		}		
-		.error {
+		.registerText {
 			color: white;
 			font-size: xx-large;
 			text-align: center;
+		}
+		.detailsText {
+			color: white;
+			font-size: x-large;
+			text-align: center;			
 		}
 		img.icon {
 			width: 20%;
@@ -23,13 +28,19 @@
 <body>
 	<?php 
 		if(!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{3}$/", $_POST["tel"])) {			
-			print('<img src="error_icon.png" alt="ikonka_bledu" class="icon">
-				<p class = "error">Podany numer telefonu jest w niewłaściwym formacie.</p>');
+			print('<img src="php_imgs/error_icon.png" alt="ikonka_bledu" class="icon">
+				<p class = "registerText">Podany numer telefonu jest w niewłaściwym formacie!</p>');
 			die("</body></html>");			
 		}
 	?>
 
-	
+	<?php
+		print("<img src=\"php_imgs/ok_icon.png\" alt=\"ikonka_ok\" class=\"icon\">
+				<p class = \"registerText\">Witaj uzytkowniku, zostałeś zarejestrowany pomyślnie!</p>");	
+		$ip = $_SERVER['REMOTE_ADDR'] ;
+        $method = $_SERVER['REQUEST_METHOD'] ;
+        print('<p class="detailsText">Twoje żądanie zostało wysłane do nas za pomocą metody '.$method.' z adresu ip o numerze '.$ip.'</p>'); 		
+	?>
 
 </body>
 </html>
