cef08f4cbb6c4be96f069f80e5e0811eef2d7f15
Grzegorz Chilczuk <chilczukg@gmail.com>
2017-12-27 21:46:12 +0100

    php part 3 - step 4: login

--- php/login_process.php
+++ php/login_process.php
@@ -1,12 +1,24 @@
 <?php
+include "db_const.php";
 session_start();
 
-$users = array('admin' => 'admin', 'user1' => 'password1');
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if (!isset($users[$username]) || $password != $users[$username]){
+if (!($database = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME))) {
+    die("Connection failed: " . mysqli_error($database));
+}
+$query = "SELECT password FROM users WHERE login = '$username'";
+if (!($result = mysqli_query($database, $query))) {
+    print ($query.'<br>');
+    die("Query execution failed: " . mysqli_error($database));
+}
+
+$row = mysqli_fetch_row($result);
+
+if ($row == null || hash('sha256', $password) != $row[0]){
     $_SESSION['logfail'] = $_POST['username'];
+    mysqli_close($database);
     header('location:login.php');
 } else {
     unset($_SESSION['logfail']);
@@ -18,6 +30,7 @@ if (!isset($users[$username]) || $password != $users[$username]){
     } else {
         $back_to .= 'index.php';
     }
+    mysqli_close($database);
     header('location:'.$back_to);
 }
 
